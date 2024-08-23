70f04f081427a292964ffb6c33bf2f7d01d0ec72
eboyfriends <142929379+eboyfriends@users.noreply.github.com>
2024-08-23 17:05:04 -0400

    done.

---
+++ ChangeFov.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
+using Microsoft.Extensions.Logging;
+
+namespace ChangeFov {
+
+    public class Main : BasePlugin {
+        
+        public override string ModuleName => "eboyfriends";
+        public override string ModuleVersion => "6.6.6";
+        public override string ModuleAuthor => "eboyfriends";
+
+        public override void Load(bool hotReload)
+        {
+            Logger.LogInformation("We are loading ChangeFov!");
+            
+            AddCommand("css_fov", "Changes the players fov", ChangeFov);
+        }
+
+        public override void Unload(bool hotReload)
+        {
+            Logger.LogInformation("We are unloading ChangeFov!");
+
+        }
+
+        private void ChangeFov(CCSPlayerController? player, CommandInfo info) {
+            if (player == null) return;
+
+            var command = info.GetArg(0);
+            var value = info.GetArg(1);
+            bool isNumber = Regex.IsMatch(value, @"^\d+$");
+
+            if (value.ToString() != null && isNumber) {
+                player.DesiredFOV = Convert.ToUInt32(value);
+                Utilities.SetStateChanged(player, "CBasePlayerController", "m_iDesiredFOV");
+            }
+            
+            player.PrintToCenter($"Fov set to {value}");
+        }
+    }
+}
